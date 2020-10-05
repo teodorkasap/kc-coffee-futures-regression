bd8fdd29ff9424c640fcdc38b0029b2c846998a9
teodorkasap <erol.erten@gmail.com>
2020-10-05 15:17:05 +0300

    more finan. feat. eng.

--- dataPrep.py
+++ dataPrep.py
@@ -1,3 +1,4 @@
+from numpy.core.function_base import add_newdoc
 from adaboost import add_EMA
 from typing import List
 import pandas as pd
@@ -104,6 +105,14 @@ def addRoc(dataframe: pd.DataFrame,
             talib.ROC(df['{}_Close'.format(commodity)], timeperiod=p)
 
 
+def addTrix(dataframe: pd.DataFrame,
+            time_periods: List[int],
+            commodity: str):
+    for p in time_periods:
+        dataframe['{}_TRIX_{}'.format(commodity, p)] = \
+            talib.TRIX(df['{}_Close'.format(commodity)], timeperiod=p)
+
+
 def addSinglePeriodFinFeat(dataframe: pd.DataFrame,
                            time_periods: List[int],
                            commodity: str,
@@ -112,20 +121,62 @@ def addSinglePeriodFinFeat(dataframe: pd.DataFrame,
                            cci=True,
                            willR=True,
                            rsi=True,
-                           roc=True):
-                           if atr == True:
-                               addAtr(dataframe, time_periods, commodity)
-                           if adx == True:
-                               addAdx(dataframe, time_periods, commodity)
-                           if cci == True:
-                               addCci(dataframe, time_periods, commodity)
-                           if willR == True:
-                               addWillR(dataframe, time_periods, commodity)
-                           if rsi == True:
-                               addRsi(dataframe, time_periods, commodity)
-                           if roc == True:
-                               addRoc(dataframe, time_periods, commodity)
+                           roc=True,
+                           trix=True):
+    if atr == True:
+        addAtr(dataframe, time_periods, commodity)
+    if adx == True:
+        addAdx(dataframe, time_periods, commodity)
+    if cci == True:
+        addCci(dataframe, time_periods, commodity)
+    if willR == True:
+        addWillR(dataframe, time_periods, commodity)
+    if rsi == True:
+        addRsi(dataframe, time_periods, commodity)
+    if roc == True:
+        addRoc(dataframe, time_periods, commodity)
+    if trix == True:
+        addTrix(dataframe, time_periods, commodity)
 
 
+def addStochSlow(dataframe: pd.DataFrame,
+                 commodity: str,
+                 fastk_period=5,
+                 slowk_period=3,
+                 slowk_matype=0,
+                 slowd_period=3,
+                 slowd_matype=0):
+    dataframe['{}_Slowk'.format(commodity)], dataframe['{}_Slowd'.format(commodity)] = \
+        talib.STOCH(dataframe['{}_High'.format(commodity)].values,
+                    dataframe['{}_Low'.format(commodity)].values,
+                    dataframe['{}_Close'.format(commodity)].values,
+                    fastk_period, slowk_period, slowk_matype,
+                    slowd_period, slowd_matype)
 
 
+def addStochFast(dataframe: pd.DataFrame,
+                 commodity: str,
+                 fastk_period=5,
+                 fastd_period=3,
+                 fastd_matype=0):
+    dataframe['{}_Fastk'.format(commodity)], dataframe['{}_Fastd'.format(commodity)] = \
+        talib.(dataframe['{}_High'.format(commodity)].values,
+               dataframe['{}_Low'.format(commodity)].values,
+               dataframe['{}_Close'.format(commodity)].values,
+               fastk_period, fastd_period, fastd_matype)
+
+
+def addUltOsc(dataframe: pd.DataFrame,
+              commodity: str,
+              timeperiod1=7,
+              timeperiod2=14,
+              timeperiod3=28):
+    """
+    adds ultimate oscillator to dataset
+    """
+    dataframe['{}_ULTOSC'.format(commodity)] = talib.ULTOSC(
+        dataframe['{}_High'.format(commodity)].values,
+        dataframe['{}_Low'.format(commodity)].values,
+        dataframe['{}_Close'.format(commodity)].values,
+        timeperiod1, timeperiod2, timeperiod3
+    )
